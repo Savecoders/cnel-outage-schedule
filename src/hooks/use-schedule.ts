38781aa4a93b494bf01c44d8c168c2756f8d97c8
Savecoders <pablopincay2003@gmail.com>
2024-11-25 19:49:18 -0500

    chore: order custom stypes and invalidateCache

--- src/hooks/use-schedule.ts
+++ src/hooks/use-schedule.ts
@@ -1,21 +1,27 @@
 import type { CriteriaSearch } from "@/enums/criteriaSearch";
-import { createQueryClient } from "@/lib/query-client";
 import scheduleTransformer from "@/lib/schedule-transformer";
 import { CnelScheduleService } from "@/services/cnel-schedule.service";
 import type { ApiSchedule } from "@/types/api-schedule.type";
 import type { TransformedSchedule } from "@/types/transformed-schedule.type";
 import {
+  type QueryClient,
   useQuery,
   type QueryKey,
   type QueryState,
 } from "@tanstack/react-query";
 
+export interface ScheduleData {
+  raw: ApiSchedule | null;
+  transformed: TransformedSchedule | null;
+}
+
 export interface ScheduleQuery {
   queryKey: QueryKey;
-  data: QueryState<TransformedSchedule | null, ApiSchedule | null> | undefined;
+  state: ScheduleData;
 }
 
 export function useSchedule(
+  queryClient: QueryClient,
   criteria?: CriteriaSearch,
   value?: string,
   options?: {
@@ -24,34 +30,52 @@ export function useSchedule(
   },
 ) {
   const DEFAULT_HOUR = 1000 * 60 * 60;
-  const queryClient = createQueryClient();
 
-  const getCachedData = (): ScheduleQuery | null | undefined => {
+  const invalidateCache = async () => {
     if (criteria && value) {
-      return queryClient.getQueryData<ScheduleQuery>([
+      await queryClient.invalidateQueries({
+        queryKey: ["schedule", criteria, value],
+      });
+    } else {
+      queryClient.invalidateQueries({
+        queryKey: ["schedule"],
+      });
+    }
+  };
+
+  const getCachedData = () => {
+    if (criteria && value) {
+      const cached = queryClient.getQueryData<ScheduleQuery>([
         "schedule",
         criteria,
         value,
       ]);
+      return cached;
     }
 
     const queries = queryClient.getQueriesData<ScheduleQuery>({
       queryKey: ["schedule"],
     });
-    if (queries.length > 0) {
-      const [latestQueryKey, latestData] = queries[queries.length - 1];
+
+    //filter valid queries
+    const validQueries = queries.filter(([_, data]) => data);
+
+    if (validQueries.length > 0) {
+      const [latestQueryKey, latestData] =
+        validQueries[validQueries.length - 1];
       return {
         queryKey: latestQueryKey,
-        data: latestData?.data,
+        state: latestData,
       };
     }
+
     return null;
   };
 
-  const query = useQuery(
+  const query = useQuery<ScheduleData | null>(
     {
       queryKey: ["schedule", criteria, value],
-      queryFn: async () => {
+      queryFn: async (): Promise<ScheduleData | null> => {
         // no criteria or value, return null
         if (!criteria || !value) return null;
 
@@ -65,10 +89,10 @@ export function useSchedule(
 
       // automatically enable the query if the criteria and value are provided
       enabled: options?.enabled ?? Boolean(criteria && value),
-      // default stale time is 1 hour
       staleTime: options?.staleTime ?? DEFAULT_HOUR,
+      refetchInterval: DEFAULT_HOUR,
       refetchOnReconnect: true, // refetch when the network reconnects
-      refetchOnMount: true, // refetch when the query is mounted
+      refetchOnMount: "always", // refetch when the query is mounted
     },
     queryClient,
   );
@@ -76,5 +100,6 @@ export function useSchedule(
   return {
     ...query,
     getCachedData,
+    invalidateCache,
   };
 }
