db105a7aed556b575c0bfd6c8991fa2b9a9e0200
Savecoders <pablopincay2003@gmail.com>
2024-11-25 19:48:20 -0500

    chore: add utils and custom query client

--- src/lib/query-client.ts
+++ src/lib/query-client.ts
@@ -13,28 +13,26 @@ const getStorage = () => {
   };
 };
 
-export function createQueryClient() {
-  const GC_TIME = 1000 * 60 * 60 * 2; // 2 hours
-  const STALE_TIME = 1000 * 60 * 60; // 1 hour
+const GC_TIME = 1000 * 60 * 60; // 1 hour
+const STALE_TIME = 1000 * 60 * 60; // 1 hour
 
-  const queryClient = new QueryClient({
-    defaultOptions: {
-      queries: {
-        gcTime: GC_TIME,
-        staleTime: STALE_TIME,
-      },
+export const customQueryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      gcTime: GC_TIME,
+      staleTime: STALE_TIME,
+      refetchOnMount: false,
+      refetchOnWindowFocus: false,
     },
-  });
+  },
+});
 
-  const localStoragePersister = createSyncStoragePersister({
-    storage: getStorage(),
-  });
+const localStoragePersister = createSyncStoragePersister({
+  storage: getStorage(),
+});
 
-  persistQueryClient({
-    queryClient,
-    persister: localStoragePersister,
-    maxAge: GC_TIME,
-  });
-
-  return queryClient;
-}
+persistQueryClient({
+  queryClient: customQueryClient,
+  persister: localStoragePersister,
+  maxAge: GC_TIME,
+});
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,15 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export function formatDate(date: string) {
+  const dateFormatted = new Date(`${date}T00:00:00`).toLocaleDateString(
+    "es-ES",
+    {
+      weekday: "long",
+      day: "2-digit",
+      month: "long",
+    },
+  );
+  return dateFormatted;
+}
