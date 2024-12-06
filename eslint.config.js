7d0aa7ec82d2d626abce8345adb72c7922a99935
Savecoders <pablopincay2003@gmail.com>
2024-12-06 12:04:03 -0500

    chore: ignore default dirs

--- eslint.config.js
+++ eslint.config.js
@@ -4,6 +4,14 @@ export default [
   // js.configs.recommended,
   ...eslintPluginAstro.configs.recommended,
   {
+    ignores: [
+      ".vscode/",
+      "node_modules/",
+      ".env",
+      ".env.local",
+      "pnpm-lock.yaml",
+      ".vercel/",
+    ],
     rules: {
       // override/add rules settings here, such as:
       // "astro/no-set-html-directive": "error"
